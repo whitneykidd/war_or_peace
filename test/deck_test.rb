360eea5fbd58746dadf1097b6a71168f230eab0e
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-16 15:57:47 -0600

    Create deck_test and setup

---
+++ test/deck_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+
+
+class DeckTest < Minitest::Test
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+  end
+end
