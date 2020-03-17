12cef2f952733ec5e9b86e5750117216e22cdf52
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-17 17:45:10 -0600

    Write turn_test setup, create Turn class, and pass test_it_exists

---
+++ lib/turn.rb
@@ -0,0 +1,6 @@
+class Turn
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+end
---
+++ test/turn_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+
+class TurnTest < Minitest::Test
+  def setup
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+
+    @turn = Turn.new(@player1, @player2)
+  end
+
+  def test_it_exists
+    assert_instance_of Turn, @turn
+  end
+end
