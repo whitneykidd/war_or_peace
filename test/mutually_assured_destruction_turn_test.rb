a1017a54cbf97662fe186c660ed017bcd63bd12c
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-19 14:30:06 -0600

    Update test names and syntax

--- test/mutually_assured_destruction_turn_test.rb
+++ test/mutually_assured_destruction_turn_test.rb
@@ -8,21 +8,21 @@ require './lib/turn'
 class MutuallyAssuredDestructionTurnTest < Minitest::Test
   def setup
     @card1 = Card.new(:heart, 'Jack', 11)
-  @card2 = Card.new(:heart, '10', 10)
-  @card3 = Card.new(:heart, '9', 9)
-  @card4 = Card.new(:diamond, 'Jack', 11)
-  @card5 = Card.new(:heart, '8', 8)
-  @card6 = Card.new(:diamond, '8', 8)
-  @card7 = Card.new(:heart, '3', 3)
-  @card8 = Card.new(:diamond, '2', 2)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, '8', 8)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
 
-  @deck1 = Deck.new([@card1, @card2, @card5, @card8])
-  @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card4, @card3, @card6, @card7])
 
-  @player1 = Player.new("Megan", @deck1)
-  @player2 = Player.new("Aurora", @deck2)
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
 
-  @turn = Turn.new(@player1, @player2)
+    @turn = Turn.new(@player1, @player2)
   end
 
   def test_it_exists
@@ -35,7 +35,11 @@ class MutuallyAssuredDestructionTurnTest < Minitest::Test
     assert_equal [], @turn.spoils_of_war
   end
 
-  def test_returns_war_type_of_turn
+  def test_returns_mutually_assured_destruction_type_of_turn
     assert_equal :mutually_assured_destruction, @turn.type
+  end
+
+  def test_returns_mutually_assured_destruction_turn_winner
+    assert_equal @player2, @turn.winner
   end
 end
