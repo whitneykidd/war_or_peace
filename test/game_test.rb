2429723bc415025e2d7cfa370e8f0db7ea1dab3c
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-22 15:52:12 -0600

    Pass test_it_has_attributes

--- test/game_test.rb
+++ test/game_test.rb
@@ -8,12 +8,17 @@ require './lib/game'
 
 class GameTest < Minitest::Test
   def setup
-    @game = Game.new(@player1, @player2)
     @player1 = Player.new('Megan', @deck1)
     @player2 = Player.new('Aurora', @deck2)
+    @game = Game.new(@player1, @player2)
   end
 
   def test_it_exists
     assert_instance_of Game, @game
+  end
+
+  def test_it_has_attributes
+    assert_equal @player1, @game.player1
+    assert_equal @player2, @game.player2
   end
 end
